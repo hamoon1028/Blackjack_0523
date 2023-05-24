63c1b9eb506fac639ca5607878b1710134aa6001
Kimsuyeub <yubk95@naver.com>
2023-05-24 16:54:21 +0900

    2023_05_24 김영진 플레이어 카드 승률 계산 추가하였습니다.

--- 0000_04_BlackJack21KKH/src/com/team21/blackjack/view/cardScreen.java
+++ 0000_04_BlackJack21KKH/src/com/team21/blackjack/view/cardScreen.java
@@ -2,25 +2,49 @@ package com.team21.blackjack.view;
 
 public class cardScreen {
 
+
+
 	public void cardScreen
 	(Object pCard, Object dCard, int cardSum, int money, int betMoney) {
 		System.out.println("==================================================\n");
 		System.out.println("\t    딜러 카드 : " + "["+dCard+"] + ??"+"\n\n");
 		System.out.printf("\t\t 배팅액 %d\n\n",betMoney);
 		System.out.printf("\t %s 카드합: %d\t 보유액 : %d\n\n",pCard,cardSum,money);
+		playerProbability(cardSum);
 		System.out.println("==================================================\n");
-		
+
 	}
-	
-	
+
+
 	public void resultcardScreen
 	(Object pCard, Object dCard, int pcardSum, int dcardSum, int money, int betMoney) {
 		System.out.println("==================================================\n");
 		System.out.printf("\t    딜러 카드 : %s  카드합: %d\n\n", dCard, dcardSum);
 		System.out.printf("\t\t 배팅액 %d\n\n",betMoney);
 		System.out.printf("\t %s 카드합: %d\t 보유액 : %d\n\n",pCard,pcardSum,money);
+		playerProbability(pcardSum);
 		System.out.println("==================================================\n");
-		
+
 	}
-	
+
+	/**
+	 * 플레이어 카드의 합을 입력받아 버스트 확률을 계산
+	 * @param player
+	 * @since 2023_05_22
+	 */
+
+	public void playerProbability(int player) {
+
+		if (player <=11 || player == 21) {
+			System.out.println("\t\t버스트 확률 : 0%");  
+		}
+		else if(player >=12 && player <=20) {
+			double bustProbability = (player - 11)*7.69;
+			System.out.printf(" \t\t버스트 확률 : %.2f%%\n", bustProbability);
+		}
+		else {
+			System.out.println("\t잘못된 카드 합입니다.");
+		}
+	}
+
 }
